52c53a4aff80e2d324c6ce8e842de9ea1d5702cf
hobarteeee123 <1113974871@qq.com>
2026-06-19 22:43:58 +0800

    prepare tcp framework

---
+++ Client/Client.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include "../Common/Protocol.h"
+#include <winsock2.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+int main()
+{
+    std::cout << "Client start\n";
+
+    return 0;
+}
